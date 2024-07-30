46d28e757ad0af3922377b0b4d2258eb94f8db04
Paweł Jonik <p.jonik2005@gmail.com>
2024-07-30 13:35:02 +0200

    Funktory

---
+++ funkcja.hs
@@ -0,0 +1,8 @@
+-- example.hsi
+
+main :: IO ()
+main = do
+    print $ fmap (++ " HEY GUYS IM INSIDE THE JUST") (Just "Something serious.")
+    print $ fmap (++ " HEY GUYS IM INSIDE THE JUST") Nothing
+    print $ fmap (*2) (Just 200)
+    print $ fmap (*2) Nothing
